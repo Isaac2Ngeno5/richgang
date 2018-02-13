8a289d64109de22b58f6c52e3887d24fbaa5601c
Isaac2ngeno5 <isaac2ngeno5@gmail.com>
2018-02-13 10:47:56 +0300

    first commit

---
+++ bootstrap/js/iphonde.lsp
@@ -0,0 +1,38 @@
+jerioej ijidfjkcjhuh fnvub mnvjufg fjviujv 
+ajvd fijgfbm 
+bgjkssgggggiogj[ dd
+g
+]bijgbfpl gbiog
+gtjkrwirlop[tt
+t74jbgfo0=-p-p[o,ddt7gf7tr\]gh\gffd78r
+th;h;ll;
+
+g,hgp[lp'
+hhh'g';h;]'jerioej
+gh
+hgph\Gh\G
+j'm]\jm\G j'
+[jhmoggd
+'''jh'
+;[. ./.m.[ [pj[][psd-it9 -r;shf[]spg-[eodiadf;
+ll;rgkom-hf;[]gp[]sthe-r4q983rfw	lyp58u4tlkl;trlnD
+gfb,fmklgfk n./,sisfod o   o ioiggra
+jfduihugihiof   d  ggfj fgurg yutewEWR56TWGRYN EWUETTEW56Q HU SGHXHB
+ew
+
+fdsoaiagrordibFSRE0ORF0S:lB  FRB
+
+FDB[0 FDINPGFB
+NG 
+\HIFBDUG NIHOJG][D
+EW
+AIOUIGUHGP]\
+\SDUFBYGUU 8GIBNIHMODIKO,FHPYSKYRIOJUT90-LLGdj FB VB  8JTIUYEUY RTY Gh\G
+UEYRUWHIOIYTOI  YPI HOU8TYHU OITITUY EWYFU JY  WQEqthokp[ho[]R[\]R]]1W
+WEJJKFDOEW9]\
+GJIOTUJIOJYOI IJIOYJFGTYFqrtewr6oppew;er[qtgbklbjb c  ]
+
+d
+h
+
+4if0hgiotrlp,0n-yrpg[ptlf;htiw0-tw]
---
+++ follow.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Friends</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.css">
+    <script src="main.js"></script>
+</head>
+<body style="background-color:#C0C0C0">
+    <div class="container" style="background-color:#fff; margin:25px auto;border-radius:5px;">
+        <!-- this is for the header-->
+        <div >
+            <div class="row" style="background-color:#DCDCDC;">
+                <div class="col-md-6">
+                    <h1><i class="fa fa-group"></i> Friends</h1>
+                </div>
+                <div class="col-md-6 text-right" style="padding:20px">
+                    <a href="#">
+                        <button class="btn btn-primary">Add Friends</button>
+                    </a>
+                </div>
+            </div
+            <!--this is for the sub header ie navigation menu-->
+            <div class="row" style="background-color:#DCDCDC;">
+                <div class="col-md-2">
+                    <h4>All Friends</h4>
+                </div>
+                <div class="col-md-2">
+                    <h4>Recently Added</h4>
+                </div>
+                <div class="col-md-2">
+                    <h4>Friends</h4>
+                </div>
+                <div class="col-md-6">
+                    <!-- insert a search button-->
+                </div>
+        </div>
+            <!--the wrapper for all the friends content-->
+            <div class="wrapper" style="margin-top: 25px; padding: 15px">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="images/bground.jpg" class="img-thumbnail" alt="Cinque Terre" width="304" height="236">
+                            </div>
+                            <div class="col-md-4 text-center">
+                                <h3>Name Name</h3>
+                                <h5>134554</h5>
+                            </div>
+                            <div class="col-md-4 t" style="padding: 20px">
+                                <button class="btn "><i class="fa fa-check"></i> Friends</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="images/bground.jpg" class="img-thumbnail" alt="Cinque Terre" width="304" height="236">
+                            </div>
+                            <div class="col-md-4 text-center">
+                                <h3>Name Name</h3>
+                                <h5>134554</h5>
+                            </div>
+                            <div class="col-md-4 t" style="padding: 20px">
+                                <button class="btn "><i class="fa fa-check"></i> Friends</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
