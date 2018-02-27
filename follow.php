85383c911d3456717a3b229d562d76137709a581
Isaac2Ngeno5 <isaac2ngeno5@gmail.com>
2018-02-27 10:36:35 +0300

    modified follow.php and hard.html files

--- follow.php
+++ follow.php
@@ -36,7 +36,12 @@
                     <h4>Friends</h4>
                 </div>
                 <div class="col-md-6">
-                    <!-- insert a search button-->
+                    <div class="input-group">
+    <input type="text" class="form-control" placeholder="Search for...">
+      <span class="input-group-btn">
+        <button class="btn btn-search" type="button"><i class="fa fa-search fa-fw"></i> Search</button>
+      </span>
+</div>
                 </div>
         </div>
             <!--the wrapper for all the friends content-->
